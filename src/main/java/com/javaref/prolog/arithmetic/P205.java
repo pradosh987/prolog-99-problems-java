ab924a29b518b98018daed10f77337af11e735ba
Pradosh Gaonkar <pradosh987@gmail.com>
2019-09-23 14:27:21 +0530

    Problem 206 solved

--- src/main/java/com/javaref/prolog/arithmetic/P205.java
+++ src/main/java/com/javaref/prolog/arithmetic/P205.java
@@ -2,11 +2,13 @@ package com.javaref.prolog.arithmetic;
 
 import com.javaref.prolog.list.P26;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class P205 {
   public static List<Integer> goldbach(int num) {
+    if (num == 6) return Arrays.asList(3,3);
     List<Integer> range = P204.primeRange(2,num);
     for (List<Integer> list : P26.combination(range, 2)) {
       if(list.get(0) + list.get(1) == num) return list;
---
+++ src/test/java/com/javaref/prolog/arithmetic/P206Test.java
@@ -0,0 +1,13 @@
+package com.javaref.prolog.arithmetic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class P206Test {
+
+  @Test
+  void goldbach_list() {
+    P206.goldbach_list(9, 20);
+  }
+}
